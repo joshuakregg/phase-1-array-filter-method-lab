f680050bd93dda93bbaf88f15c3cc85253898472
joshau kregg <kreggjoshua@gmail.com>
2023-03-21 13:13:53 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(collection, driver) {
+    return collection.filter(user => user.toLowerCase() === driver.toLowerCase());
+}
+
+function fuzzyMatch(collection, driver) {
+    return collection.filter(user => user[0] === driver[0]);
+}
+
+function matchName(collection, driver) {
+    return collection.filter(user => user.name.toLowerCase() === driver.toLowerCase());
+}
